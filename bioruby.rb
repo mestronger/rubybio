aed279f1b27d4d8a8fa3e41c855903b9da559de0
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-23 05:07:59 +0700

    Create bioruby.rb

---
+++ bioruby.rb
@@ -0,0 +1,20 @@
+# Mengumpulkan informasi dari pengguna
+print "Nama: "
+nama = gets.chomp
+
+print "Usia: "
+usia = gets.chomp
+
+print "Alamat: "
+alamat = gets.chomp
+
+print "Pekerjaan: "
+pekerjaan = gets.chomp
+
+# Mencetak biodata ke layar
+puts "===== BIODATA ====="
+puts "Nama: Dian"
+puts "Usia: 15 tahun"
+puts "Alamat: penjaringan, jakarta utara"
+puts "Pekerjaan: programmmer"
+end
