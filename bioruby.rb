08a866a79afd5b3ceff17fc2e3fd936655cceedc
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-23 05:21:47 +0700

    Update bioruby.rb

--- bioruby.rb
+++ bioruby.rb
@@ -1,17 +1,3 @@
-# Mengumpulkan informasi dari pengguna
-print "Nama: "
-nama = gets.chomp
-
-print "Usia: "
-usia = gets.chomp
-
-print "Alamat: "
-alamat = gets.chomp
-
-print "Pekerjaan: "
-pekerjaan = gets.chomp
-
-# Mencetak biodata ke layar
 puts "===== BIODATA ====="
 puts "Nama: Dian"
 puts "Usia: 15 tahun"
