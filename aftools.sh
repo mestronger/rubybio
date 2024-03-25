02db812e262e7c08d87c6c0faa5a29a523f72690
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-25 10:56:11 +0700

    Update and rename bioruby.rb to aftools.sh

---
+++ aftools.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+
+echo "
+▄▀█ █▀▀   ▀█▀ █▀█ █▀█ █░░ █▀
+█▀█ █▀░   ░█░ █▄█ █▄█ █▄▄ ▄█"
+
+echo "1. facebook analysic"
+echo "2. facebook adress check"
+echo "3. facebook login cokie"
+
+echo "Pilih menu 1/2/3 :"
--- bioruby.rb
+++
@@ -1,6 +0,0 @@
-puts "===== BIODATA ====="
-puts "Nama: Dian"
-puts "Usia: 15 tahun"
-puts "Alamat: penjaringan, jakarta utara"
-puts "Pekerjaan: programmmer"
-end
